923ba969e493dcf7b59e762b1cafffabefda4088
YouuInKeun <keun0390@naver.com>
2019-09-03 22:27:20 +0900

    [ADD] Project 뼈대 완성

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\keun0\onedrive\바탕 화면\창업 경진대회\todo\todo-app\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
