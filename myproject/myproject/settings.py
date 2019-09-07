780f5dc254375348b69613fcb6f43284ada0b513
YouuInKeun <keun0390@naver.com>
2019-09-08 00:14:19 +0900

    [ADD] django.config 수정

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '#y&ibfi(a8weo&$h6_h+b!h*91%)dj!6r4k^$zz7#b#^n$f7&#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# 'django-env.vhegrgfbnh.us-west-2.elasticbeanstalk.com'
+ALLOWED_HOSTS = ['django-env.f34r3y75vc.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
